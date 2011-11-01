940bab31806802dd14d53668bda3c66b194cfbce
Daniel Lew <danlew42@gmail.com>
2011-11-01 16:26:19 -0500

    Added basic project

---
+++ src/com/idunnolol/mvcdemo/TabletActivity.java
@@ -0,0 +1,12 @@
+package com.idunnolol.mvcdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TabletActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
